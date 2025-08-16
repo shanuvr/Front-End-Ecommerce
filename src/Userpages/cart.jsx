ed1bb6e3ba267b1cba7870d53aca5edb462004cc
shanuvrdev <shanuvr112233@gmail.com>
2025-08-16 11:16:43 +0530

    cart listed

--- src/Userpages/cart.jsx
+++ src/Userpages/cart.jsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 import UserLayout from "../Layout/UserLayout";
 import { useNavigate } from "react-router-dom";
 import api from "../api/axios";
+import { DumbbellIcon } from "lucide-react";
 
 export default function CartPage() {
     const user = localStorage.getItem('user')
-    const [carts,setCarts] =useState({})
+    const [carts,setCarts] =useState([])
+    const [cartitems,setCartItems]=useState([])
     const navigate = useNavigate()
     useEffect(()=>{
         
@@ -14,17 +16,31 @@ export default function CartPage() {
     },[])
     async function getCart() {
       const user = await api.get('/cart')
-      console.log(user.data.cart[0]);
+      // console.log(user.data.cart[0]);
       setCarts(user.data.cart[0])
-      console.log(carts);
+      setCartItems(user.data.cart[0].cartitems)
+      // console.log(carts);
       
 
     }
 useEffect(() => {
- 
-      console.log("Carts state has been updated:", carts.cartitems);
+      console.log("Carts :", carts);
+      console.log(carts.total);
+      
     
   }, [carts]);
+  async function handleDelete(id) {
+    console.log(id);
+    const del = await api.delete(`/cart/${id}`)
+    getCart()
+  }
+  async function handleOrder() {
+    console.log("check out worked");
+    
+    const odr=  await api.post('/order')
+
+    
+  }
   return (
     <UserLayout>
         {user ? (
@@ -32,21 +48,28 @@ useEffect(() => {
       <div className="md:col-span-2">
         <h1 className="text-2xl font-bold mb-4">Shopping Cart</h1>
 
-
-                   <div className="flex items-center border-b py-4">
+        {
+          cartitems.map((ele,ind)=>{
+            return(
+                <div className="flex items-center border-b py-4">
           <img
-            src="https://via.placeholder.com/100"
+            src={`http://localhost:3000/${ele.image}`}
             alt="product"
             className="w-24 h-24 object-cover"
           />
           <div className="ml-4 flex-1">
-            <h2 className="font-semibold">Basic Tee</h2>
-            <p className="text-gray-500">Sienna | Large</p>
-            <p className="font-semibold">$32.00</p>
-            <p className="text-green-500 text-sm">In stock</p>
+            <h2 className="font-semibold">Product name: {ele.name}</h2>
+            <p className="text-gray-500">{ele.description}</p>
+            <p className="text-gray-500"> Quantity {ele.quantity}</p>
+            <p className="font-semibold">Price {ele.price}</p>
+            <p className="font-semibold"> Total {ele.subtotal}</p>
           </div>
-          <button className="ml-4 text-gray-500">X</button>
+          <button onClick={()=>{handleDelete(ele.productId)}} className="ml-4 text-gray-500">X</button>
         </div>
+            )
+          })
+        }
+           
             
 
        
@@ -57,23 +80,20 @@ useEffect(() => {
 
       <div className="border p-4 rounded">
         <h2 className="text-lg font-semibold mb-4">Order summary</h2>
+       
         <div className="flex justify-between py-1">
-          <span>Subtotal</span>
-          <span>$99.00</span>
-        </div>
-        <div className="flex justify-between py-1">
-          <span>Shipping estimate</span>
+          <span>Delivery Charge</span>
           <span>$5.00</span>
         </div>
         <div className="flex justify-between py-1">
-          <span>Tax estimate</span>
+          <span>GST</span>
           <span>$8.32</span>
         </div>
         <div className="flex justify-between font-bold border-t mt-2 pt-2">
           <span>Order total</span>
-          <span>{}</span>
+          <span>{carts.total}</span>
         </div>
-        <button className="w-full bg-purple-600 text-white py-2 rounded mt-4">
+        <button onClick={()=>{handleOrder()}} className="w-full bg-purple-600 text-white py-2 rounded mt-4">
           Checkout
         </button>
       </div>
